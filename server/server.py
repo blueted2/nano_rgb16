a0c430b379f19071cd7e2a9c7369d8d6a3ab220e
Gregory Holder <gregory.holder76@gmail.com>
2023-12-08 13:33:15 +0100

    asdf

---
+++ server/server.py
@@ -0,0 +1,83 @@
+from flask import Flask, Response, request, send_file
+import cv2
+import threading
+import time
+import os
+
+app = Flask(__name__)
+
+# Shared variable for storing the latest frame
+latest_frame = None
+frame_lock = threading.Lock()
+
+# Initialize the webcam capture
+cap = cv2.VideoCapture(0)
+
+def camera_thread():
+    global latest_frame
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        with frame_lock:
+            latest_frame = frame
+
+        time.sleep(0.01)
+
+def capture_frame(format='jpeg', width=None, height=None):
+    with frame_lock:
+        if latest_frame is not None:
+            # Resize the frame if width and height are provided
+            if width and height:
+                frame = cv2.resize(latest_frame, (int(width), int(height)))
+            else:
+                frame = latest_frame
+
+            # Encode the frame in the requested format
+            ret, buffer = cv2.imencode(f'.{format}', frame)
+            return buffer.tobytes()
+        else:
+            return None
+
+@app.route('/capture')
+def capture():
+    # Get format and resolution from the request arguments
+    format = request.args.get('format', 'jpeg')
+    width = request.args.get('width')
+    height = request.args.get('height')
+
+    frame = capture_frame(format, width, height)
+    if frame:
+        return Response(frame, mimetype=f'image/{format}')
+    else:
+        return "Failed to capture image", 500
+    
+
+@app.route('/', methods = ["POST"])
+def upload():
+
+    if request.content_type != "image/jpeg":
+        return "invalid format", 400
+    
+    with open("img.jpeg", "bw") as f:
+        f.write(request.get_data())
+
+    return "", 200
+
+
+@app.route('/', methods = ["GET"])
+def download():
+    if os.path.isfile("img.jpeg"):
+        return send_file("img.jpeg")
+    
+    return "no image uploaded", 404
+    
+
+if __name__ == '__main__':
+    threading.Thread(target=camera_thread, daemon=True).start()
+
+    try:
+        app.run(debug=False, threaded=True, port=3002, host="0.0.0.0")
+    except KeyboardInterrupt:
+        cap.release()
--- webcam_rgb888/webcam_rgb888.ino
+++ webcam_rgb888/webcam_rgb888.ino
@@ -4,11 +4,9 @@
 #define IMG_WIDTH 120
 #define IMG_HEIGHT 100
 
-uint8_t buf1[IMG_WIDTH * IMG_HEIGHT];
-uint8_t buf2[IMG_WIDTH * IMG_HEIGHT];
-
-uint8_t *curr_buf = buf1;
-uint8_t *prev_buf = buf2;
+#define SERVER "192.168.238.193"
+#define PORT 3002
+#define URL "/?width=120&height=100&format=bmp"
 
 struct rgb565_t {
   uint16_t data;
@@ -16,6 +14,18 @@ struct rgb565_t {
   uint8_t get_r() { return (data >> 11) & 0b11111; }
   uint8_t get_g() { return (data >> 5) & 0b111111; }
   uint8_t get_b() { return (data)&0b11111; }
+
+  uint8_t get_r_norm() { return (data >> 8) & 0b11111000; }
+  uint8_t get_g_norm() { return (data >> 3) & 0b11111100; }
+  uint8_t get_b_norm() { return (data << 3) & 0b11111000; }
+
+  static rgb565_t from_rgb888(uint8_t r, uint8_t g, uint8_t b) {
+    r = r >> 3;
+    g = g >> 2;
+    b = b >> 3;
+
+    return rgb565_t{(r << 11) | (g << 5) | b};
+  }
 };
 
 struct rgb332_t {
@@ -24,6 +34,10 @@ struct rgb332_t {
   uint8_t get_r() { return (data >> 5) & 0b111; }
   uint8_t get_g() { return (data >> 2) & 0b111; }
   uint8_t get_b() { return (data)&0b11; }
+
+  uint8_t get_r_norm() { return (data << 0) & 0b11100000; }
+  uint8_t get_g_norm() { return (data << 3) & 0b11100000; }
+  uint8_t get_b_norm() { return (data << 6) & 0b11000000; }
 
   static rgb332_t from_rgb565(rgb565_t pixel) {
     uint8_t r = pixel.get_r() >> 2;
@@ -44,11 +58,22 @@ struct rgb332_t {
   static uint8_t sq_dist(rgb332_t left, rgb332_t right) {
     uint8_t r_diff = abs((left.get_r() - right.get_r()));
     uint8_t g_diff = abs((left.get_g() - right.get_g()));
-    uint8_t b_diff = abs((left.get_b() - right.get_b())) * 2; // make sure all three channels occupy the same number of bits (3)
+
+    // make sure all three channels occupy the same number of bits (3)
+    uint8_t b_diff = abs((left.get_b() - right.get_b())) * 2;
 
     return r_diff * r_diff + g_diff + g_diff + b_diff * b_diff;
   }
 };
+
+rgb565_t buf1[IMG_WIDTH * IMG_HEIGHT];
+// uint8_t buf2[IMG_WIDTH * IMG_HEIGHT];
+
+rgb565_t *curr_buf = buf1;
+rgb565_t *prev_buf = buf1;
+
+WiFiClient client;
+HttpClient http(client);
 
 void setup() {
   Serial.begin(9600);
@@ -57,6 +82,11 @@ void setup() {
   while (WiFi.status() == WL_NO_MODULE) {
     Serial.println("// No communication with WiFi module");
   }
+
+  client.setTimeout(100);
+  client.setRetry(false);
+
+  http = HttpClient(client);
 }
 
 void loop() {
@@ -69,11 +99,7 @@ void loop() {
     Serial.println("connecting to wifi");
   }
 
-  WiFiClient client;
-
-  HttpClient http(client);
-
-  if (http.get("192.168.238.193", 3000, "/capture") == 0) {
+  if (http.get(SERVER, PORT, URL) == 0) {
 
     int err = http.skipResponseHeaders();
     int bodyLen = http.contentLength();
@@ -81,73 +107,106 @@ void loop() {
     char header[54];
     http.readBytes(header, 54);
 
-    
     uint32_t width = IMG_WIDTH;
     uint32_t height = IMG_HEIGHT;
 
     uint32_t _width = *(uint32_t *)(header + 0x12);
-    uint32_t _height = -*(int32_t *)(header + 0x16); // negative indicates "top-down" order
+    int32_t _height = *(int32_t *)(header + 0x16);
 
-    if(width != _width || height != _height) {
+    if (width != _width || height != abs(_height)) {
       Serial.println("Wrong image size, aborting!");
+      Serial.println(_width);
+      Serial.println(_height);
+      http.stop();
       return;
     }
-    
 
-    read_888_as_332(http, (rgb332_t *)curr_buf);
- 
-    for (int j = 0; j < height; j += 4) {
-      for (int i = 0; i < width; i += 2) {
-        uint8_t old_y = prev_buf[j * width + i];
-        rgb332_t pix = rgb332_t {old_y};
+    if (!read_888_as_565(http, curr_buf, _height > 0)) {
+      Serial.println("Failed to retrieve image");
+      http.stop();
+      return;
+    }
 
-        Serial.print(set_serial_color(pix.get_r() << 5, pix.get_g() << 5, pix.get_b() << 6));
-        Serial.print(" ");
+    for (int j = 0; j < height; j += 2) {
+      // for (int i = 0; i < width; i += 2) {
+      //   uint8_t old_y = prev_buf[j * width + i];
+      //   rgb332_t pix = rgb332_t {old_y};
+
+      //   Serial.print(set_serial_color(pix.get_r() << 5, pix.get_g() << 5,
+      //   pix.get_b() << 6)); Serial.print(" ");
+      // }
+
+      for (int i = 0; i < width; i += 1) {
+        rgb565_t pix_top = curr_buf[j * width + i];
+        rgb565_t pix_bottom = curr_buf[(j + 1) * width + i];
+
+        // rgb332_t pix_top = rgb332_t{y};
+        // rgb332_t pix_bottom = rgb332_t{y2};
+
+        uint8_t r_top = pix_top.get_r_norm();
+        uint8_t g_top = pix_top.get_g_norm();
+        uint8_t b_top = pix_top.get_b_norm();
+
+        uint8_t r_bottom = pix_bottom.get_r_norm();
+        uint8_t g_bottom = pix_bottom.get_g_norm();
+        uint8_t b_bottom = pix_bottom.get_b_norm();
+
+        print_two_pixels(r_top, g_top, b_top, r_bottom, g_bottom, b_bottom);
+
+        // Serial.print(set_serial_color(pix.get_r() << 5, pix.get_g() << 5,
+        // pix.get_b() << 6)); Serial.print(" ");
       }
 
+      // for (int i = 0; i < width; i += 2) {
+      //   uint8_t y = curr_buf[j * width + i];
+      //   uint8_t old_y = prev_buf[j * width + i];
 
-      for (int i = 0; i < width; i += 2) {
-        uint8_t y = curr_buf[j * width + i];
+      //   rgb332_t pix = rgb332_t {y};
+      //   rgb332_t old_pix = rgb332_t {old_y};
 
-        rgb332_t pix = rgb332_t {y};
-        Serial.print(set_serial_color(pix.get_r() << 5, pix.get_g() << 5, pix.get_b() << 6));
-        Serial.print(" ");
-      }
+      //   // uint8_t y_dist_sq = (y - old_y) * (y - old_y) / 256;
 
-      for (int i = 0; i < width; i += 2) {
-        uint8_t y = curr_buf[j * width + i];
-        uint8_t old_y = prev_buf[j * width + i];
+      //   uint8_t y_dist_sq = rgb332_t::sq_dist(pix, old_pix);
 
-        rgb332_t pix = rgb332_t {y};
-        rgb332_t old_pix = rgb332_t {old_y};
+      //   // max value of sq_dist for rgb332 is 147 (3 * 7^2)
+      //   y_dist_sq = (float(y_dist_sq) / 147 * 256);
 
-     
-        // uint8_t y_dist_sq = (y - old_y) * (y - old_y) / 256;
-
-        uint8_t y_dist_sq = rgb332_t::sq_dist(pix, old_pix);
-
-        // max value of sq_dist for rgb332 is 147 (3 * 7^2)
-        y_dist_sq = (float(y_dist_sq) / 147 * 256);
-
-        
-
-        if (y_dist_sq > 200) {
-          Serial.println(y_dist_sq);
-        }
-        Serial.print(set_serial_color(y_dist_sq, y_dist_sq, y_dist_sq));
-        Serial.print(" ");
-      }
+      //   // if (y_dist_sq > 200) {
+      //   //   Serial.println(y_dist_sq);
+      //   // }
+      //   // Serial.print(set_serial_color(y_dist_sq, y_dist_sq, y_dist_sq));
+      //   // Serial.print(" ");
+      // }
       Serial.println();
     }
 
-
     // swap buffers
-    uint8_t *tmp = curr_buf;
+    rgb565_t *tmp = curr_buf;
     curr_buf = prev_buf;
     prev_buf = tmp;
 
+  } else {
+    Serial.println("http error");
   }
   http.stop();
+}
+
+void print_two_pixels(uint8_t r_top, uint8_t g_top, uint8_t b_top,
+                      uint8_t r_bottom, uint8_t g_bottom, uint8_t b_bottom) {
+  Serial.print("\033[38;2;");
+  Serial.print(r_top);
+  Serial.print(";");
+  Serial.print(g_top);
+  Serial.print(";");
+  Serial.print(b_top);
+  Serial.print("m");
+  Serial.print("\033[48;2;");
+  Serial.print(r_bottom);
+  Serial.print(";");
+  Serial.print(g_bottom);
+  Serial.print(";");
+  Serial.print(b_bottom);
+  Serial.print("m▀");
 }
 
 String set_serial_color(uint8_t r, uint8_t g, uint8_t b) {
@@ -175,13 +234,43 @@ void read_888_as_332(Stream &stream, rgb332_t *buf) {
     for (int x = 0; x < IMG_WIDTH; x++) {
       uint8_t bgr[3];
 
-      stream.readBytes(bgr, 3);
+      if (stream.readBytes(bgr, 3) != 3) {
+        return;
+      }
 
       rgb332_t pixel332 = rgb332_t::from_rgb888(bgr[2], bgr[1], bgr[0]);
 
       *buf = pixel332;
       buf++;
     }
+    print_two_pixels(255, 255, 255, 255, 255, 255);
   }
+  Serial.println();
 }
 
+bool read_888_as_565(Stream &stream, rgb565_t *buf, bool backwards) {
+
+  for (int line = 0; line < IMG_HEIGHT; line++) {
+    for (int x = 0; x < IMG_WIDTH; x++) {
+
+      int actual_line = line;
+      if (backwards) {
+        actual_line = IMG_HEIGHT - line - 1;
+      }
+
+      int index = actual_line * IMG_WIDTH + x;
+      
+      uint8_t bgr[3];
+
+      if (stream.readBytes(bgr, 3) != 3) {
+        return false;
+      }
+
+      rgb565_t pixel565 = rgb565_t::from_rgb888(bgr[2], bgr[1], bgr[0]);
+
+      buf[index] = pixel565;
+
+    }
+  }
+  return true;
+}
